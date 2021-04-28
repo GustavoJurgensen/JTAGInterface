b60a5a406efaa9963717d23e236d4a98ba9c9c27
Sevothart <santos.lucasmmatheus@gmail.com>
2021-04-28 14:23:22 -0300

    Beautify

--- interface/src/signalOperators.py
+++ interface/src/signalOperators.py
@@ -1,4 +1,5 @@
 from random import sample
+
 def sweep(array):
     buffer=[]
     for i in range(0,len(array)):
@@ -11,7 +12,8 @@ def sweep(array):
         st += i
     return st
 
-def bitsToChar(array):#convert bits to character
+# convert bits to character
+def bitsToChar(array):
     integer = 0
     c = ''
     for i in range(0,7):
@@ -19,7 +21,9 @@ def bitsToChar(array):#convert bits to character
     c = chr(integer)
     #print(chr(integer))
     return c  
-def charToBits(character):#convert Character to bits
+
+# convert Character to bits
+def charToBits(character):
     decNumber = ord(character)
     array = []
     while decNumber > 0:
@@ -27,17 +31,22 @@ def charToBits(character):#convert Character to bits
         array.append(rem)
         decNumber = decNumber // 2
     size = len(array)
-    for i in range(0,7):#additional bits, in the ASC-2 table,7 is the maximun bits can be used
+    # additional bits, in the ASC-2 table
+    # 7 is the maximun bits that can be used
+    for i in range(0,7):
         if(i<(7-size)):
             array.append(0)
     #print(array)
     array.reverse()
     return array
-def stringToBinary(fileName):#convert any "string" to binary array 
+
+# convert any "string" to binary array
+def stringToBinary(fileName): 
     aux = []
     aux2 = []
     array = []
-    with open(fileName,"r") as txt:# open file where are "strings"
+    # open file where are "strings"
+    with open(fileName,"r") as txt:
         for i in txt:
             aux.append(i)
     for i in aux:
@@ -51,7 +60,7 @@ def stringToBinary(fileName):#convert any "string" to binary array
             array.append(j)
     file_ = open(fileName,"w")
     file_.write(st)
-    return array#return binary array
+    return array #return binary array
 
 def fileToString(fileName):
     array=[]
@@ -59,13 +68,17 @@ def fileToString(fileName):
         for i in original:
             array.append(i)
     return array
-def convertToSquare(array):#convert binary wave for binary square wave
+
+# convert binary wave for binary square wave
+def convertToSquare(array):
     aux = []
     for i in array:
         for j in range(0,20):
             aux.append(i)
     return aux
-def NRZ_L(array, reverse):#apply NRZ-L coding 
+
+# apply NRZ-L coding
+def NRZ_L(array, reverse): 
     after=[]
     if reverse == False:#encoding
         for i in array:
@@ -80,14 +93,17 @@ def NRZ_L(array, reverse):#apply NRZ-L coding
             else:
                 after.append(1)
     return after
-def convert_txt(fileName):#convet Txt file
+
+# convet Txt file
+def convert_txt(fileName):
     before=[]
     after=[]
     before = stringToBinary(fileName)#original signal
     after = NRZ_L(before,False)#signal encoded by NRZ_L 
-
     return before,after
-def add_noise(array, percent):#add noise according to a percentage
+
+# add noise according to a percentage
+def add_noise(array, percent):
 
     mult =int(len(array)*percent)
     error = sample(range(0,len(array)),mult)
@@ -100,7 +116,9 @@ def add_noise(array, percent):#add noise according to a percentage
         else:
             array[i]=1
     return array
-def writeNoise(array):#write in a file the signal after apply noise
+
+# write in a file the signal after apply noise
+def writeNoise(array):
     array = NRZ_L(array, True)
     buffer = []
     ret = []
